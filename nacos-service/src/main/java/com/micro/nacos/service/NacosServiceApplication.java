36c0b0189b555b369304dd13f896c6695b2ad462
jigang.mao <mjgang2016@163.com>
2020-12-18 22:34:13 +0800

    微服务测试init

---
+++ nacos-service/src/main/java/com/micro/nacos/service/NacosServiceApplication.java
@@ -0,0 +1,15 @@
+package com.micro.nacos.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lenovo on 2020/12/18.
+ */
+@SpringBootApplication
+public class NacosServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosServiceApplication.class, args);
+    }
+}
